14a652a9d81263166ee376267e59ec6aa6ff94f9
ACER <ACER@DESKTOP-CJBAJUG>
2023-04-26 19:33:50 +0400

    SignIn function added

--- CapitalShopFinalProject/Controllers/AccountController.cs
+++ CapitalShopFinalProject/Controllers/AccountController.cs
@@ -12,12 +12,13 @@ namespace CapitalShopFinalProject.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        public AccountController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _roleManager = roleManager;
-
+            _signInManager = signInManager;
         }
 
 
@@ -65,7 +66,7 @@ namespace CapitalShopFinalProject.Controllers
             await _userManager.AddToRoleAsync(appUser, "Member");
 
            
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction(nameof(SignIn));
             
         }
 
@@ -78,10 +79,40 @@ namespace CapitalShopFinalProject.Controllers
 
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignIn(SignInVM signInVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(signInVM);
+            }
+
+            AppUser appUser = await _userManager.FindByEmailAsync(signInVM.Email);
+
+            if (appUser == null)
+            {
+                ModelState.AddModelError("", "Email or Password is not correct");
+                return View(signInVM);
+            }
 
 
 
+            Microsoft.AspNetCore.Identity.SignInResult signInResult = await _signInManager.PasswordSignInAsync(appUser, signInVM.Password, true, true);
+
+            if (!signInResult.Succeeded)
+            {
+                ModelState.AddModelError("", "Email or Password is not correct");
+                return View(signInVM);
+
+            }
 
 
+
+            return RedirectToAction("Index", "Home");
+
+
+
+        }
     }
 }
---
+++ CapitalShopFinalProject/ViewModels/AccountVM/SignInVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapitalShopFinalProject.ViewModels.AccountVM
+{
+    public class SignInVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
